d6eb5ad193d60a33d335d165ccddce0281369b97
mollymillk <kristey.z39@gmail.com>
2021-07-28 12:46:54 +0300

    create webpack files

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const CssMinimizerWebpackPligin = require('css-minimizer-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin =require('mini-css-extract-plugin')
+
+ 
+module.exports = {
+	mode: 'development',
+	entry: './compomemts/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'scripts.js'
+	},
+	module: {
+		rules: [
+		  {
+			test: /\.(scss|css)$/,
+			use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+		  },
+		  {
+			test: /\.(svg|png|gif|jpg)$/,
+			exclude: /fonts/,
+			loader: 'file-loader',
+			options: {
+				name: '[name].[ext]'
+			}
+		  },
+		  {
+			test: /\.(ttf|eot|woff|svg|woff2)$/,
+			use: {
+			  loader: "file-loader",
+			  options: {
+				name: '[name].[ext]',
+				outputPath: 'fonts/'
+			  }
+			}
+		  },
+		  	{
+			test: /\.pug$/,
+			loader: 'pug-loader',
+			options: {
+				pretty: true
+				}
+			}
+		]
+	  },
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'compomemts/pug_comp/index.pug'
+		}),
+		new CssMinimizerWebpackPligin(),
+		new MiniCssExtractPlugin(),
+
+	],
+	optimization: {
+		minimize: true,
+	  },
+	
+}
