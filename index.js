56d53a07dd1807f2cd9e996be44e5f7718b173c3
mollymillk <kristey.z39@gmail.com>
2021-08-04 11:04:00 +0300

    changed index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import "./src/asserts/fonts/fonts.scss";
 import "./src/components/cards/date/date_card.scss";
 import "./src/components/cards/checkout/checkout_card.scss";
+import $ from 'jquery';
+ 
+global.jQuery = $;
+global.$ = $;
