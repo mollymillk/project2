260616d842007661ef74952d9e1a9d9def79dc19
mollymillk <kristey.z39@gmail.com>
2021-08-09 13:36:02 +0300

    added txt field

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import "./src/asserts/fonts/fonts.scss";
 import "./src/components/cards/date/date_card.scss";
 import "./src/components/cards/checkout/checkout_card.scss";
 import $ from 'jquery';
+import "./src/components/form_elements/txt_field/txt_field.scss";
+
  
 global.jQuery = $;
 global.$ = $;
