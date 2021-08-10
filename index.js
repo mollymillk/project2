7ebaa13ad6237c72717d1de61564dcd5ef9af32f
mollymillk <kristey.z39@gmail.com>
2021-08-10 13:59:58 +0300

    add button

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import "./src/asserts/fonts/fonts.scss";
 import "./src/components/cards/date/date_card.scss";
 import "./src/components/cards/checkout/checkout_card.scss";
-import $ from 'jquery';
 import "./src/components/form_elements/txt_field/txt_field.scss";
 import "./src/components/form_elements/dropdown/dropdown.scss";
 import "./src/components/form_elements/masked_text_field/masked_text_field.scss";
 import "./src/components/form_elements/date_dropdown/date_dropdown.scss";
+import "./src/components/form_elements/buttons/buttons.scss";
 
 
 
 
+import $ from 'jquery';
  
 global.jQuery = $;
 global.$ = $;
