841142a5f467aff77e7136d4be7fe5113f1f5c2a
mollymillk <kristey.z39@gmail.com>
2021-10-22 13:30:56 +0300

    add sliderto all cards

--- src/components/cards/room/room_card.js
+++ src/components/cards/room/room_card.js
@@ -1,8 +1,59 @@
+// var allSlides = document.getElementsByClassName(".container");
 
-const slider = new NSlider({ 
-    elem: document.querySelector('.container'),
+// for (var i = 0; i < allSlides.length; i++) {
+    // var allSlides = document.getElementByClassName('container1')
+    var slider = new NSlider({ 
+         elem: document.querySelector('.room_1'),
+         dots: true,
+         prevButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
+         nextButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
+    });
+    var slider = new NSlider({ 
+        elem: document.querySelector('.room_2'),
+        dots: true,
+        prevButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
+        nextButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
+   });
+   var slider = new NSlider({ 
+    elem: document.querySelector('.room_3'),
+    dots: true,
+    prevButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
+    nextButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
+});
+var slider = new NSlider({ 
+    elem: document.querySelector('.room_4'),
+    dots: true,
+    prevButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
+    nextButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
+});
+var slider = new NSlider({ 
+    elem: document.querySelector('.room_5'),
+    dots: true,
+    prevButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
+    nextButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
+});
+var slider = new NSlider({ 
+    elem: document.querySelector('.room_6'),
+    dots: true,
+    prevButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
+    nextButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
+});
+var slider = new NSlider({ 
+    elem: document.querySelector('.room_7'),
     dots: true,
     prevButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
     nextButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
 });
 
+//    var slider = new NSlider({ 
+//     elem: document.querySelector('.3'),
+//     dots: true,
+//     prevButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
+//     nextButtonInner: '<svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.42188 10.5938L6.01563 12L0.0156253 6L6.01563 2.38311e-07L7.42188 1.40625L2.82813 6L7.42188 10.5938Z" fill="white"/></svg>',
+// });
+
+// var allSlides = document.getElementsByClassName(".room_photos");
+
+// for (var i = 0; i < allSlides.length; i++) {
+//     slider
+// }
