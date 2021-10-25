40856457fb6f5109c626f2ae03d7377004e48f15
mollymillk <kristey.z39@gmail.com>
2021-10-25 13:37:10 +0300

    change cards

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
 	entry: './index.js',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: '[hash].bundle.js',
+		filename: 'bundle.js',
 	},
 	target: "web",
 	devServer: {
